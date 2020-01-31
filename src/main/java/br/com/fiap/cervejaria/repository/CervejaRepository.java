b1ea740e0b2d015d3801405d4749bd4610cefbda
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2020-01-30 23:04:54 -0300

    spring data

--- src/main/java/br/com/fiap/cervejaria/repository/CervejaRepository.java
+++ src/main/java/br/com/fiap/cervejaria/repository/CervejaRepository.java
@@ -1,8 +1,21 @@
 package br.com.fiap.cervejaria.repository;
 
+import br.com.fiap.cervejaria.dto.Tipo;
 import br.com.fiap.cervejaria.entity.Cerveja;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CervejaRepository extends JpaRepository<Cerveja, Integer>{
+
+    Page<Cerveja> findAll(Pageable pageable);
+
+    List<Cerveja> findAllByTipo(Tipo tipo);
+
+    @Query("from Cerveja c where c.tipo = :tipo")
+    List<Cerveja> findPorTipo(Tipo tipo);
 
 }
