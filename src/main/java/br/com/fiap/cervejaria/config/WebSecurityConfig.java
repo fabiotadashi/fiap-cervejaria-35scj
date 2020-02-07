fae5834cdb3a6d7a257bdf63e52a570f7e72b7b4
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2020-02-06 22:14:45 -0300

    security

---
+++ src/main/java/br/com/fiap/cervejaria/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package br.com.fiap.cervejaria.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}adminsenha").roles("ADMIN", "USER")
+            .and()
+                .withUser("user").password("{noop}usersenha").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "*/cervejas").hasRole("ADMIN")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+}
