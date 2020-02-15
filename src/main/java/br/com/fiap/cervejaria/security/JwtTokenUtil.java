3aead3de223577d60dc95724c890eb82da94dd73
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2020-02-15 09:34:37 -0300

    token util

--- src/main/java/br/com/fiap/cervejaria/security/JwtTokenUtil.java
+++ src/main/java/br/com/fiap/cervejaria/security/JwtTokenUtil.java
@@ -1,7 +1,15 @@
 package br.com.fiap.cervejaria.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.Date;
+import java.util.HashMap;
 
 @Component
 public class JwtTokenUtil {
@@ -11,5 +19,29 @@ public class JwtTokenUtil {
 
     @Value("${jwt.expiration}")
     private int expire;
+
+    public String generateToken(String username){
+        Date dataCriacao = getDateFromLocalDateTime(LocalDateTime.now());
+        Date dataExpiracao = getDateFromLocalDateTime(LocalDateTime.now().plusMinutes(expire));
+        return Jwts.builder()
+                .setClaims(new HashMap<>())
+                .setSubject(username)
+                .setIssuedAt(dataCriacao)
+                .setExpiration(dataExpiracao)
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token.replace("Bearer ",""))
+                .getBody();
+        return claims.getSubject();
+    }
+
+    private Date getDateFromLocalDateTime(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.toInstant(OffsetDateTime.now().getOffset()));
+    }
 
 }
